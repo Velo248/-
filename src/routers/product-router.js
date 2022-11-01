874b12d922794b4dfe1196c39c20943c53e0d3e5
chuuchoi <lrour153@naver.com>
2022-11-02 00:11:22 +0900

    feat: Product patch API added

--- src/routers/product-router.js
+++ src/routers/product-router.js
@@ -26,7 +26,7 @@ productRouter.get("/products",async(req,res,next)=>{
     }
 })
 
-//상품 추가
+//상품 추가 API - /api/product
 productRouter.post("/product",async(req,res,next)=>{
     try{
         // Content-Type: application/json 설정을 안 한 경우, 에러를 만들도록 함.
@@ -52,8 +52,6 @@ productRouter.post("/product",async(req,res,next)=>{
             temp.push(c)
         }
       }
-      console.log(temp)
-      console.log("----+++++++=========================----------------------------")
       for(let c of temp){
         await categoryService.addCategory({category_name:c})
       }
@@ -66,6 +64,36 @@ productRouter.post("/product",async(req,res,next)=>{
     // 추가된 상품의 db 데이터를 프론트에 다시 보내줌
     // 물론 프론트에서 안 쓸 수도 있지만, 편의상 일단 보내 줌
     res.status(201).json(newProduct);
+    }catch(error){
+        next(error)
+    }
+})
+
+//상품 수정 API - /api/products/:productId
+productRouter.patch("/products/:productId",async(req,res,next)=>{
+    try{
+        // Content-Type: application/json 설정을 안 한 경우, 에러를 만들도록 함.
+        // application/json 설정을 프론트에서 안 하면, body가 비어 있게 됨.
+        if (is.emptyObject(req.body)) {
+            throw new Error(
+                "headers의 Content-Type을 application/json으로 설정해주세요"
+            );
+        }
+        const productId = req.params.productId
+        const {product_no,product_name,price,brand,specification,categories}=req.body
+
+      // 위 데이터가 undefined가 아니라면, 즉, 프론트에서 업데이트를 위해
+      // 보내주었다면, 업데이트용 객체에 삽입함.
+      const toUpdate = {
+        ...(product_no && { product_no }),
+        ...(product_name && { product_name }),
+        ...(price && { price }),
+        ...(brand && { brand }),
+        ...(specification && { specification }),
+        ...(categories && {categories})
+      };
+       const updatedProuct = await productService.updateProduct(productId,toUpdate)
+       res.status(200).json(updatedProuct)
     }catch(error){
         next(error)
     }
