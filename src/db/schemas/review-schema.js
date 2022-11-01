6a2aac5286f84b3627bcb05c9b55eb22c9a071f1
chuuchoi <lrour153@naver.com>
2022-11-01 21:17:52 +0900

    set: review-schema setup

---
+++ src/db/schemas/review-schema.js
@@ -0,0 +1,16 @@
+import { Schema } from'mongoose';
+const ReviewSchema = new Schema({
+  // content, String, required,
+  content:{type:String,required:true},
+  // author, User, required
+  star:{type:Number},
+  author:{
+      type:Schema.Types.ObjectId,
+      ref:'users',
+      required:true
+  },
+}, {
+  timestamps: true,
+});
+
+export { ReviewSchema };
