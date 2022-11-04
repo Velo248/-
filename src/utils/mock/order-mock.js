6d61201d6bf9e917b0bb2028f39dc14eeae2aa9c
chuuchoi <lrour153@naver.com>
2022-11-04 10:54:47 +0900

    styled: email form & export type modified

--- src/utils/mock/order-mock.js
+++ src/utils/mock/order-mock.js
@@ -1,4 +1,4 @@
-export const orderMockData = () => [
+export const orderMockData = [
   {
     address: {
       postalCode: '36752',
@@ -7,15 +7,11 @@ export const orderMockData = () => [
       receiverName: '어드민1의 형의 형',
       receiverPhoneNumber: '01022123222',
     },
-    _id: '6363d55de59d4cfdb3a7fe48',
     userId: '63631e0435889784ad12ab03',
     summaryTitle: '파란 모자 / 1개',
     totalPrice: 13000,
     request: '부재 시 문 앞에 놓아주세요.',
     status: '상품 배송 준비중',
-    createdAt: '2022-11-03T14:51:09.104Z',
-    updatedAt: '2022-11-03T14:51:09.104Z',
-    __v: 0,
   },
   {
     address: {
@@ -25,15 +21,11 @@ export const orderMockData = () => [
       receiverName: '어드민1의 형',
       receiverPhoneNumber: '01022123222',
     },
-    _id: '6363d567e59d4cfdb3a7fe4a',
     userId: '63631e0435889784ad12ab03',
     summaryTitle: '검은 모자 / 2개',
     totalPrice: 13000,
     request: '부재 시 문 앞에 놓아주세요.',
     status: '상품 배송 준비중',
-    createdAt: '2022-11-03T14:51:19.346Z',
-    updatedAt: '2022-11-03T14:51:19.346Z',
-    __v: 0,
   },
   {
     address: {
@@ -43,15 +35,11 @@ export const orderMockData = () => [
       receiverName: '어드민1의 형',
       receiverPhoneNumber: '01022123222',
     },
-    _id: '6363d56be59d4cfdb3a7fe4c',
     userId: '63631e0435889784ad12ab03',
     summaryTitle: '검은 모자 / 3개',
     totalPrice: 13000,
     request: '부재 시 문 앞에 놓아주세요.',
     status: '상품 배송 준비중',
-    createdAt: '2022-11-03T14:51:23.627Z',
-    updatedAt: '2022-11-03T14:51:23.627Z',
-    __v: 0,
   },
   {
     address: {
@@ -61,15 +49,11 @@ export const orderMockData = () => [
       receiverName: '어드민1의 형',
       receiverPhoneNumber: '01022123222',
     },
-    _id: '6363d574e59d4cfdb3a7fe4e',
     userId: '63631e0435889784ad12ab03',
     summaryTitle: '검은 티셔츠 / 1개',
     totalPrice: 13000,
     request: '부재 시 문 앞에 놓아주세요.',
     status: '상품 배송 준비중',
-    createdAt: '2022-11-03T14:51:32.929Z',
-    updatedAt: '2022-11-03T14:51:32.929Z',
-    __v: 0,
   },
   {
     address: {
@@ -79,15 +63,11 @@ export const orderMockData = () => [
       receiverName: '엘리스',
       receiverPhoneNumber: '01022123222',
     },
-    _id: '6363d5aae59d4cfdb3a7fe50',
     userId: '6363d4359fb75d3f41bd53a2',
     summaryTitle: ' 신발 / 1개',
     totalPrice: 13000,
     request: '부재 시 문 앞에 놓아주세요.',
     status: '상품 배송 준비중',
-    createdAt: '2022-11-03T14:52:26.882Z',
-    updatedAt: '2022-11-03T14:52:26.882Z',
-    __v: 0,
   },
   {
     address: {
@@ -97,15 +77,11 @@ export const orderMockData = () => [
       receiverName: '엘리스1 ',
       receiverPhoneNumber: '01022123222',
     },
-    _id: '6363d5afe59d4cfdb3a7fe52',
     userId: '6363d4359fb75d3f41bd53a2',
     summaryTitle: ' 신발 / 1개',
     totalPrice: 13000,
     request: '부재 시 문 앞에 놓아주세요.',
     status: '상품 배송 준비중',
-    createdAt: '2022-11-03T14:52:31.741Z',
-    updatedAt: '2022-11-03T14:52:31.741Z',
-    __v: 0,
   },
   {
     address: {
@@ -115,15 +91,11 @@ export const orderMockData = () => [
       receiverName: '엘리스1 ',
       receiverPhoneNumber: '01022123222',
     },
-    _id: '6363d5b3e59d4cfdb3a7fe54',
     userId: '6363d4359fb75d3f41bd53a2',
     summaryTitle: ' 파란 신발 / 1개',
     totalPrice: 13000,
     request: '부재 시 문 앞에 놓아주세요.',
     status: '상품 배송 준비중',
-    createdAt: '2022-11-03T14:52:35.087Z',
-    updatedAt: '2022-11-03T14:52:35.087Z',
-    __v: 0,
   },
   {
     address: {
@@ -133,15 +105,11 @@ export const orderMockData = () => [
       receiverName: '엘리스1 ',
       receiverPhoneNumber: '01022123222',
     },
-    _id: '6363d5b8e59d4cfdb3a7fe56',
     userId: '6363d4359fb75d3f41bd53a2',
     summaryTitle: ' 빨강 신발 / 1개',
     totalPrice: 13000,
     request: '부재 시 문 앞에 놓아주세요.',
     status: '상품 배송 준비중',
-    createdAt: '2022-11-03T14:52:40.585Z',
-    updatedAt: '2022-11-03T14:52:40.585Z',
-    __v: 0,
   },
   {
     address: {
@@ -151,14 +119,10 @@ export const orderMockData = () => [
       receiverName: '엘리스1 ',
       receiverPhoneNumber: '01022123222',
     },
-    _id: '6363d5bce59d4cfdb3a7fe58',
     userId: '6363d4359fb75d3f41bd53a2',
     summaryTitle: ' 유니크 신발 / 1개',
     totalPrice: 13000,
     request: '부재 시 문 앞에 놓아주세요.',
     status: '상품 배송 준비중',
-    createdAt: '2022-11-03T14:52:44.419Z',
-    updatedAt: '2022-11-03T14:52:44.419Z',
-    __v: 0,
   },
 ];
