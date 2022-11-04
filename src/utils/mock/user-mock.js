1b80b9e43afdfe255ed8bbb615e44e08a1f4cd35
조 건형 <qwes3123@gmail.com>
2022-11-04 17:54:17 +0900

    fix: /mockDataGeneratorBugfix

--- src/utils/mock/user-mock.js
+++ src/utils/mock/user-mock.js
@@ -1,6 +1,12 @@
 //비밀번호 1234 고정 admin 계정 맨 밑꺼
 export const userMockData = [
   {
+    email: 'admin1@test.com',
+    fullName: 'admin1',
+    password: '$2b$10$YNI87UIFk86lXvDtBwO/8.NLIe3uCxZtzlEOO/lAv9EIGWtUOELaW',
+    role: 'basic-user',
+  },
+  {
     email: 'elice1@test.com',
     fullName: 'elice1',
     password: '$2b$10$wU9qHZSxOFiWX5mGiW7v/OrqADlSv2SUY8MxvW5rEI55vNGCDJY5q',
@@ -16,12 +22,6 @@ export const userMockData = [
     email: 'elice3@test.com',
     fullName: 'elice3',
     password: '$2b$10$LprHvew8FjJClAuw4VAGn.Ly/Rp3esHoeennEgvaQ2vj3usvdZTm2',
-    role: 'basic-user',
-  },
-  {
-    email: 'admin1@test.com',
-    fullName: 'admin1',
-    password: '$2b$10$YNI87UIFk86lXvDtBwO/8.NLIe3uCxZtzlEOO/lAv9EIGWtUOELaW',
     role: 'basic-user',
   },
 ];
