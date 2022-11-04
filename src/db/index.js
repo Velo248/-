c10f9ccae8c3e2e414643a034331dc876c4a3014
조 건형 <qwes3123@gmail.com>
2022-11-04 13:47:00 +0900

    fix: file name change

--- src/db/index.js
+++ src/db/index.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
-import { mockGenerator } from '../utils/data-pull';
+import { mockGenerator } from '../utils/mock-generator';
 const DB_URL =
   process.env.MONGODB_URL ||
   'MongoDB 서버 주소가 설정되지 않았습니다.\n./db/index.js 파일을 확인해 주세요. \n.env 파일도 필요합니다.\n';
