36d25341108c05392830d161920bc189e22d1abe
Velo248 <jhpark4884@gmail.com>
2022-11-07 01:15:52 +0900

    feat: admin product list -> js

--- src/views/pages/admin-product-list/main.js
+++ src/views/pages/admin-product-list/main.js
@@ -1,6 +1,96 @@
 const $admin_product_wapper = document.querySelector('.admin_product_wapper');
+const $products = document.querySelector('.products');
 
-const getProduct = async () => {};
-$admin_product_wapper.addEventListener('click', () => {});
+const getProduct = async () => {
+  const respose = await fetch('/api/products');
+  return await respose.json();
+};
+
+const getCategory = async () => {
+  const respose = await fetch('/api/categories');
+  return await respose.json();
+};
+const elementCreater = (current, add) => {
+  current.innerHTML += add;
+};
+const pageRender = async () => {
+  const responseProducts = await getProduct();
+  const responseCategorise = await getCategory();
+  const products = getCategoryTitleFromId(responseProducts, responseCategorise);
+
+  products.forEach((product) => {
+    const { _id, title, categoryTitle, shortDescription, manufacturer, price } =
+      product;
+
+    const html_temp = `
+      <div class='product_item' data-key=${_id}>
+        <div>
+          <span>${title}</span>
+        </div>
+        <div>
+          <span>${categoryTitle}</span>
+        </div>
+        <div>
+          <span>${shortDescription}</span>
+        </div>
+        <div>
+          <span>${manufacturer}</span>
+        </div>
+        <div>
+          <span>${price}</span>
+        </div>
+        <div>
+          <button class='detail_bnt'>상세</button>
+        </div>
+      </div>
+    `;
+
+    elementCreater($products, html_temp);
+  });
+};
+
+const getCategoryTitleFromId = (product, category) => {
+  let categoryObj = {};
+  const newProduct = [];
+
+  category.forEach((categorise) => {
+    const { _id, title } = categorise;
+    categoryObj[_id] = title;
+  });
+
+  product.forEach((products) => {
+    const tempProduct = { ...products };
+    tempProduct.categoryTitle = categoryObj[tempProduct.categoryId];
+    newProduct.push(tempProduct);
+  });
+
+  return newProduct;
+};
+
+const dateFormet = (date) => {
+  return `${date.substring(0, 10)} ${date.substring(11, 16)}`;
+};
+
+const productDetail = (productId) => {
+  sessionStorage.setItem('p_id', productId);
+  window.location.href = '/admin-product-detail';
+};
+
+const clickEventMap = {
+  detail_bnt(e) {
+    productDetail(e.parentNode.parentNode.getAttribute('data-key'));
+  },
+};
+
+$admin_product_wapper.addEventListener('click', (e) => {
+  // console.log(e.target);
+  if (!clickEventMap[e.target.className]) return;
+
+  clickEventMap[e.target.className](e.target);
+});
 $admin_product_wapper.addEventListener('change', () => {});
 $admin_product_wapper.addEventListener('submit', () => {});
+
+window.addEventListener('DOMContentLoaded', async () => {
+  await pageRender();
+});
