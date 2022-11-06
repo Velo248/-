2de5225039921ee5a08e191fc4c3c4b52e5cee0f
Velo248 <jhpark4884@gmail.com>
2022-11-06 21:22:18 +0900

    feat: admin product -> js setup

---
+++ src/views/pages/admin-product-list/main.js
@@ -0,0 +1,6 @@
+const $admin_product_wapper = document.querySelector('.admin_product_wapper');
+
+const getProduct = async () => {};
+$admin_product_wapper.addEventListener('click', () => {});
+$admin_product_wapper.addEventListener('change', () => {});
+$admin_product_wapper.addEventListener('submit', () => {});
