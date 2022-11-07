736dd6c6319c2d6bb07f2e252d9994e32aa97b49
Velo248 <jhpark4884@gmail.com>
2022-11-08 01:08:22 +0900

    fix: admin main -> 불필요 css제거, 경로수정

--- src/views/pages/admin-main/main.js
+++ src/views/pages/admin-main/main.js
@@ -2,10 +2,10 @@ const $admin_work_container = document.querySelectorAll(
   '.admin_work_container',
 );
 const linkMap = {
-  0: '/admin-category-list',
-  1: '/admin-product-list',
-  2: '/admin-user-list',
-  3: '/admin-order-list',
+  0: '/admin/category/list',
+  1: '/admin/product/list',
+  2: '/admin/user/list',
+  3: '/admin/order/list',
 };
 
 $admin_work_container.forEach((e, idx) => {
