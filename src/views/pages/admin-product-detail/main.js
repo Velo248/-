aa911251506a77d72125dfe236dffe7c12d9eb3c
Velo248 <jhpark4884@gmail.com>
2022-11-08 02:46:32 +0900

    fix: product detail -> html, refactoring Js

--- src/views/pages/admin-product-detail/main.js
+++ src/views/pages/admin-product-detail/main.js
@@ -1,28 +1,25 @@
+import { elementCreater, dateFormet } from '/public/scripts/util.js';
+import productService from '/public/scripts/productService.js';
+import categoryService from '/public/scripts/categoryService.js';
+
 const $product_detail_wapper = document.querySelector('.product_detail_wapper');
 const $product = document.querySelector('.product');
 
+const categoryObj = {};
+
 const getProduct = async () => {
-  const response = await fetch(
-    `/api/products/${sessionStorage.getItem('p_id')}`,
+  const response = await productService.getProductByProductId(
+    sessionStorage.getItem('p_id'),
   );
-  return await response.json();
+  return response;
 };
 
 const getCategory = async () => {
-  const respose = await fetch('/api/categories');
-  return await respose.json();
+  const response = await categoryService.getAllCategories();
+  return response;
 };
 
-const elementCreater = (current, add) => {
-  current.innerHTML += add;
-};
-
-const dateFormet = (date) => {
-  return `${date.substring(0, 10)} ${date.substring(11, 16)}`;
-};
-
-const getCategoryTitleFromId = (product, category) => {
-  let categoryObj = {};
+const setCategoryObj = (product, category) => {
   const tempProduct = { ...product };
 
   category.forEach((categorise) => {
@@ -35,17 +32,32 @@ const getCategoryTitleFromId = (product, category) => {
   return tempProduct;
 };
 
+const setSelectOption = (target, selected) => {
+  Object.keys(categoryObj).forEach((category) => {
+    if (category === selected) {
+      const temp_html = `
+      <option selected value="${categoryObj[category]}">${categoryObj[category]}</option>
+    `;
+      elementCreater(target, temp_html);
+    } else {
+      const temp_html = `
+      <option value="${categoryObj[category]}">${categoryObj[category]}</option>
+    `;
+      elementCreater(target, temp_html);
+    }
+  });
+};
+
 const pageRender = async () => {
   const productResponse = await getProduct();
   const categoryResponse = await getCategory();
 
-  const product = getCategoryTitleFromId(productResponse, categoryResponse);
+  setCategoryObj(productResponse, categoryResponse);
 
   const {
     _id,
     sellerId,
     title,
-    categoryTitle,
     categoryId,
     manufacturer,
     shortDescription,
@@ -58,7 +70,7 @@ const pageRender = async () => {
     discountPercent,
     createdAt,
     updatedAt,
-  } = product;
+  } = productResponse;
 
   const createDate = dateFormet(createdAt);
   const updateDate = dateFormet(updatedAt);
@@ -72,7 +84,7 @@ const pageRender = async () => {
         <input name='sellerId' value="${sellerId}">
       </div>
       <div>
-        <input name='categoryTitle' value="${categoryTitle}" readonly>
+        <select class="category_select" name="category_select"></select>
       </div>
       <div>
         <input name='manufacturer' value="${manufacturer}">
@@ -111,15 +123,16 @@ const pageRender = async () => {
   `;
 
   elementCreater($product, temp_html);
+  setSelectOption(document.querySelector('.category_select'), categoryId);
 };
 
 const deleteProduct = async () => {
-  const data = {
-    target: sessionStorage.getItem('p_id'),
-    dataObj: {},
-    method: 'DELETE',
-  };
-  if (confirm('진심 삭제함?')) return await customFetcher(data);
+  if (confirm('진심 삭제함?')) {
+    await productService.deleteProductByProductId(
+      sessionStorage.getItem('p_id'),
+    );
+    return (location.href = '/admin/product/list');
+  }
   return;
 };
 
@@ -127,8 +140,13 @@ const editProduct = async () => {
   const $edit_form = document.querySelector('.edit_form');
   const formData = new FormData($edit_form);
 
-  //카테고리ID <-> 카테고리title 매칭추가 예정
+  const productId = $edit_form.getAttribute('id');
+  const categoryId = Object.keys(categoryObj).find(
+    (key) => categoryObj[key] === formData.get('category_select'),
+  );
+
   const updateObj = {};
+  updateObj.categoryId = categoryId;
   updateObj.title = formData.get('title');
   updateObj.sellerId = formData.get('sellerId');
   updateObj.manufacturer = formData.get('manufacturer');
@@ -141,15 +159,9 @@ const editProduct = async () => {
   updateObj.isRecommended = formData.get('isRecommended');
   updateObj.discountPercent = formData.get('discountPercent');
 
-  const data = {
-    target: $edit_form.getAttribute('id'),
-    dataObj: updateObj,
-    method: 'PATCH',
-  };
-
-  await customFetcher(data);
+  await productService.setProductInfomation(productId, updateObj);
   sessionStorage.removeItem('p_id');
-  window.location.href = '/admin-product-list';
+  location.href = '/admin/product/list';
 };
 
 const clickEventMap = {
@@ -163,33 +175,11 @@ const clickEventMap = {
 };
 
 $product_detail_wapper.addEventListener('click', (e) => {
+  if (!clickEventMap[e.target.className]) return;
+
   clickEventMap[e.target.className](e.target);
 });
 
 window.addEventListener('DOMContentLoaded', async () => {
   await pageRender();
 });
-
-//리팩토링때, 분리예정
-const customFetcher = async (data) => {
-  const { target, dataObj, method } = data;
-
-  const res = await fetch(`/api/products/${target}`, {
-    method: `${method}`,
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${sessionStorage.getItem('token')}`,
-    },
-    body: JSON.stringify(dataObj),
-  });
-
-  if (!res.ok) {
-    const errorContent = await res.json();
-    const { reason } = errorContent;
-
-    throw new Error(reason);
-  }
-
-  sessionStorage.removeItem('p_id');
-  window.location.href = '/admin-product-list';
-};
