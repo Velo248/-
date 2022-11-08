a754f098575a2628b59bfdedc4e8f0dbd431072d
BB2e <79270369+BB2e@users.noreply.github.com>
2022-11-09 05:13:06 +0900

    feat: css 레이아웃 통일

--- src/views/pages/pay-history/main.js
+++ src/views/pages/pay-history/main.js
@@ -15,11 +15,14 @@ const createHistoryDiv = ({ _id, summaryTitle, totalPrice, status }) => {
   historyDetail.className = 'flex-justify-between payment_history_detail';
   historyDetail.dataset.id = _id;
   const historyBody = `
-    <div class="order_number">
-    <a href="/orders/${_id}">${_id}</a></div>
-    <div class="order_title">${summaryTitle}</div>
-    <div class="order_status">${status}</div>
-    <div class="order_total_price">${totalPrice.toLocaleString('ko-kr')}원</div>
+    <span class="order_number">
+      <a href="/orders/${_id}">${_id}</a>
+    </span>
+    <span class="order_title">${summaryTitle}</span>
+    <span class="order_status">${status}</span>
+    <span class="order_total_price">${totalPrice.toLocaleString(
+      'ko-kr',
+    )}원</span>
     `;
   historyDetail.innerHTML = historyBody;
 
