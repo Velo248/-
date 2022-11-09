3dd09f26c60b853c50746eb6df028d96b6222676
조 건형 <qwes3123@gmail.com>
2022-11-10 01:31:36 +0900

    refactor: addAsyncHandler

--- src/routers/cart-router.js
+++ src/routers/cart-router.js
@@ -1,25 +1,28 @@
 import { Router } from 'express';
 import is from '@sindresorhus/is';
 // 폴더에서 import하면, 자동으로 폴더의 index.js에서 가져옴
-import { isAdmin, loginRequired } from '../middlewares';
+import { loginRequired } from '../middlewares';
 import { cartService } from '../services';
+import { asyncHandler } from '../utils/async-handler';
 
 const cartRouter = Router();
 
 //전체 장바구니 조회 API - GET /api/carts
-cartRouter.get('/carts', loginRequired, async (req, res, next) => {
-  try {
+cartRouter.get(
+  '/carts',
+  loginRequired,
+  asyncHandler(async (req, res) => {
     const { currentUserId } = req;
     console.log(currentUserId);
     const carts = await cartService.getCartByUserId(currentUserId);
     res.status(200).json(carts);
-  } catch (error) {
-    next(error);
-  }
-});
+  }),
+);
 //장바구니 추가 API - POST /cart
-cartRouter.post('/carts', loginRequired, async (req, res, next) => {
-  try {
+cartRouter.post(
+  '/carts',
+  loginRequired,
+  asyncHandler(async (req, res) => {
     const { currentUserId } = req;
     const { orderSheets } = req.body;
     const newCart = await cartService.addOrderSheets({
@@ -29,14 +32,14 @@ cartRouter.post('/carts', loginRequired, async (req, res, next) => {
     // 추가된 장바구니 db 데이터를 프론트에 다시 보내줌
     // 물론 프론트에서 안 쓸 수도 있지만, 편의상 일단 보내 줌
     res.status(201).json({ newCart });
-  } catch (error) {
-    next(error);
-  }
-});
+  }),
+);
 
 //장바구니에 추가 API - PATCH /cart/{cartId}
-cartRouter.patch('/carts', loginRequired, async (req, res, next) => {
-  try {
+cartRouter.patch(
+  '/carts',
+  loginRequired,
+  asyncHandler(async (req, res) => {
     const { currentUserId } = req;
     const { productId, quantity } = req.body;
     if (!productId) {
@@ -58,14 +61,14 @@ cartRouter.patch('/carts', loginRequired, async (req, res, next) => {
       toUpdate,
     );
     res.status(200).json(updatedCart);
-  } catch (error) {
-    next(error);
-  }
-});
+  }),
+);
 
 //장바구니 삭제 API - DELETE /cart/{cartId}
-cartRouter.delete('/carts', loginRequired, async (req, res, next) => {
-  try {
+cartRouter.delete(
+  '/carts',
+  loginRequired,
+  asyncHandler(async (req, res) => {
     const { productId } = req.body;
     const { currentUserId } = req;
     if (productId) {
@@ -79,23 +82,23 @@ cartRouter.delete('/carts', loginRequired, async (req, res, next) => {
     const deletedCart = await cartService.deleteCartAll(currentUserId);
     res.status(200).json(deletedCart);
     return;
-  } catch (error) {
-    next(error);
-  }
-});
+  }),
+);
 
 //아래는 잘되는지 테스팅용 코드 위에는 일일이 register,login 과정을 거쳐야하기때문
-cartRouter.get('/carts/:userId', loginRequired, async (req, res, next) => {
-  try {
+cartRouter.get(
+  '/carts/:userId',
+  loginRequired,
+  asyncHandler(async (req, res) => {
     const { userId } = req.params;
     const carts = await cartService.getCartByUserId(userId);
     res.status(200).json(carts);
-  } catch (error) {
-    next(error);
-  }
-});
-cartRouter.post('/carts/:userId', loginRequired, async (req, res, next) => {
-  try {
+  }),
+);
+cartRouter.post(
+  '/carts/:userId',
+  loginRequired,
+  asyncHandler(async (req, res) => {
     const { userId } = req.params;
     const { orderSheets } = req.body;
     const newCart = await cartService.addOrderSheets({
@@ -105,9 +108,7 @@ cartRouter.post('/carts/:userId', loginRequired, async (req, res, next) => {
     // 추가된 장바구니 db 데이터를 프론트에 다시 보내줌
     // 물론 프론트에서 안 쓸 수도 있지만, 편의상 일단 보내 줌
     res.status(201).json({ newCart });
-  } catch (error) {
-    next(error);
-  }
-});
+  }),
+);
 
 export { cartRouter };
--- src/routers/send-mail-router.js
+++ src/routers/send-mail-router.js
@@ -1,16 +1,21 @@
 import sendMail from '../utils/send-mail';
 import { Router } from 'express';
+import { asyncHandler } from '../utils/async-handler';
+
 const sendMailRouter = Router();
-sendMailRouter.post('/send-mail', async (req, res, next) => {
-  const { email } = req.body;
-  if (!email) {
-    res
-      .status(400)
-      .json('req.body.email undefined, 이메일을 요청 바디에 적어주세요');
-    return;
-  }
-  await sendMail(email);
-  res.status(201).json('메일이 발송되었습니다.');
-});
+sendMailRouter.post(
+  '/send-mail',
+  asyncHandler(async (req, res) => {
+    const { email } = req.body;
+    if (!email) {
+      res
+        .status(400)
+        .json('req.body.email undefined, 이메일을 요청 바디에 적어주세요');
+      return;
+    }
+    await sendMail(email);
+    res.status(201).json('메일이 발송되었습니다.');
+  }),
+);
 
 export { sendMailRouter };
