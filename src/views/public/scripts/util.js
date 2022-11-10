b2a3bb3880e24344a25122da578759d7d390f88c
Velo248 <jhpark4884@gmail.com>
2022-11-10 16:21:59 +0900

    fix: Add errorUtil

--- src/views/public/scripts/util.js
+++ src/views/public/scripts/util.js
@@ -45,3 +45,28 @@ export const setLocalStorageItem = (itemKey, data) => {
  * @returns {number} - 콤마를 제거하고 10진수로 변환한 숫자
  */
 export const parsePrice = (str) => parseInt(str.replace(/,/g, ''), 10);
+
+export const errorUtil = {
+  invalidVariable: (data) => {
+    if (!data) {
+      return false;
+    }
+    return true;
+  },
+
+  isValidEmail: (email) => {
+    const reg =
+      /([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+    return reg.test(email);
+  },
+
+  isValidPW: (password, confirmPassword) => {
+    if (password.length < 8)
+      return { code: false, err: 'password longer then 8' };
+    if (password !== confirmPassword)
+      return { code: false, err: 'password Not match' };
+
+    const reg = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+    return { code: reg.test(password) };
+  },
+};
