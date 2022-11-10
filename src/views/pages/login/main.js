9052f3941d41abe7ba297c316994b1e9b42c9a22
Velo248 <jhpark4884@gmail.com>
2022-11-10 22:01:36 +0900

    fix: login page 로직수정

--- src/views/pages/login/main.js
+++ src/views/pages/login/main.js
@@ -1,31 +1,52 @@
 import userService from '/public/scripts/userService.js';
+import { errorUtil } from '/public/scripts/util.js';
 
-const $loginEmail = document.querySelector('.login-email');
-const $loginPassword = document.querySelector('.login-pw');
-const $loginBtn = document.querySelector('.login-btn');
+const $loginForm = document.querySelector('.box-style-center');
 
-// 로그인 요청
-$loginBtn.addEventListener('click', async (e) => {
-  e.preventDefault();
+const login = async (data) => {
+  const formData = new FormData(data);
+  const email = formData.get('email');
+  const password = formData.get('password');
 
-  const email = $loginEmail.value;
-  const password = $loginPassword.value;
+  const invalidVariable = errorUtil.invalidVariable(
+    [email, password].every((v) => v),
+  );
+
+  if (!invalidVariable) return alert('입력정보를 확인해주세요.');
 
   const response = await userService.login(email, password);
+  const { isAdmin, token, isDormantAccount, isPasswordUpdateNeeded } = response;
 
-  if (response.isAdmin && response.token) {
-    sessionStorage.setItem('token', response.token);
-    sessionStorage.setItem('isAdmin', response.isAdmin);
+  if (isAdmin) {
+    sessionStorage.setItem('token', token);
+    sessionStorage.setItem('isAdmin', isAdmin);
 
     alert('관리자 유저 로그인 성공');
     location.href = '/admin';
     return;
   }
 
-  sessionStorage.setItem('token', response.token);
-  sessionStorage.setItem('isAdmin', response.isAdmin);
-  location.href = '/';
+  //이메일 발송로직 추가
+  if (isDormantAccount.value) {
+    alert('장기 미접속으로 휴면계정으로 바뀌었습니다. 이메일을 확인해주세요.');
+    return;
+  }
 
-  $loginEmail.value = '';
-  $loginPassword.value = '';
+  sessionStorage.setItem('token', token);
+  sessionStorage.setItem('isAdmin', isAdmin);
+  sessionStorage.setItem(
+    'isPasswordUpdateNeeded',
+    isPasswordUpdateNeeded.value,
+  );
+  location.href = '/';
+};
+
+const submitEventMap = {
+  login: login,
+};
+
+$loginForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  if (!submitEventMap[e.target.className]) return;
+  submitEventMap[e.target.className](e.target);
 });
