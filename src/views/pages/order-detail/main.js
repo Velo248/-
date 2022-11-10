7df494322f2444bf87b8039e0028d5f818ee7a78
BB2e <79270369+BB2e@users.noreply.github.com>
2022-11-10 21:03:56 +0900

    feat: 유저페이지 css 공통작업

--- src/views/pages/order-detail/main.js
+++ src/views/pages/order-detail/main.js
@@ -6,13 +6,21 @@ const createOrderRow = (orderId, { summaryTitle, totalPrice, status }) => {
   orderRow.className = 'row';
   orderRow.innerHTML = `
         <div class="order_number">
+          <b>주문번호</b>
           <span class="order_number_detail">${orderId}</span>
         </div>
-        <span class="order_summary">${summaryTitle}</span>
-        <span class="column order_status">${status}</span>
-        <span class="column order_price">${totalPrice.toLocaleString(
-          'ko-kr',
-        )}</span>
+        <div>
+          <b>주문 내역</b>
+          <span class="order_summary">${summaryTitle}</span>
+        </div>
+        <div>
+          <b>주문 상태</b>
+          <span class="order_status">${status}</span>
+        </div>
+        <div>
+          <b>금액</b>
+          <span class="order_price">${totalPrice.toLocaleString('ko-kr')}</span>
+        </div>
     `;
   return orderRow;
 };
@@ -31,19 +39,23 @@ const createDeliveryInfo = ({
   deliveryInfo.classname = 'delivery_info';
   deliveryInfo.innerHTML = `
             <em>배송 정보</em>
-            <div>
+            <div class="row">
               <div class="line">
+                <b>받는 사람</b>
                 <span class="receiver_name">${receiverName}</span>
               </div>
               <div class="line">
+                <b>주소</b>
                 <span class="receiver_address"
                   >${postalCode + ' ' + address1 + ' ' + address2}</span
                 >
               </div>
               <div class="line">
+                <b>연락처</b>
                 <span class="receiver_phone_number">${receiverPhoneNumber}</span>
               </div>
               <div class="line">
+                <b>배송 상태</b>
                 <span class="delivery_status">${status}</span>
               </div>`;
   return deliveryInfo;
