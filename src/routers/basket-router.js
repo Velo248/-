2e64d8933311f39cca6b4b24e79f6fbdefdc5c21
조 건형 <qwes3123@gmail.com>
2022-11-11 16:20:35 +0900

    refactor: add basket controller

--- src/routers/basket-router.js
+++ src/routers/basket-router.js
@@ -1,65 +1,20 @@
 import { Router } from 'express';
-import is from '@sindresorhus/is';
-// 폴더에서 import하면, 자동으로 폴더의 index.js에서 가져옴
-import { basketService } from '../services';
+import { basketController } from '../controllers';
 import { loginRequired } from '../middlewares';
-import { asyncHandler } from '../utils/async-handler';
 
 const basketRouter = Router();
-basketRouter.get(
-  '/baskets',
-  loginRequired,
-  asyncHandler(async (req, res) => {
-    const { currentUserId } = req;
-    const baskets = await basketService.getBasketsByUserId(currentUserId);
-    res.status(200).json({ baskets });
-  }),
-);
 
-basketRouter.post(
-  '/baskets',
-  loginRequired,
-  asyncHandler(async (req, res) => {
-    const { currentUserId } = req;
-    const { orderSheets } = req.body;
-    const baskets = await basketService.addBasket(currentUserId, orderSheets);
-    res.status(200).json({ baskets });
-  }),
-);
-basketRouter.patch(
-  '/baskets',
-  loginRequired,
-  asyncHandler(async (req, res) => {
-    const { currentUserId } = req;
-    const { orderSheets } = req.body;
-    const baskets = await basketService.updateBasket(
-      currentUserId,
-      orderSheets,
-    );
+basketRouter.get('/baskets', loginRequired, basketController.getBaskets);
 
-    res.status(200).json({ baskets });
-  }),
-);
+basketRouter.post('/baskets', loginRequired, basketController.addBasket);
 
-basketRouter.delete(
-  '/baskets',
-  loginRequired,
-  asyncHandler(async (req, res) => {
-    const { currentUserId } = req;
-    const baskets = await basketService.deleteBaskets(currentUserId);
+basketRouter.patch('/baskets', loginRequired, basketController.updateBasket);
 
-    res.status(200).json({ baskets });
-  }),
-);
+basketRouter.delete('/baskets', loginRequired, basketController.deleteBaskets);
 basketRouter.delete(
   '/baskets/:basketId',
   loginRequired,
-  asyncHandler(async (req, res) => {
-    const { currentUserId } = req;
-    const { basketId } = req.params;
-    const basket = await basketService.deleteBasket(currentUserId, basketId);
-
-    res.status(200).json({ basket });
-  }),
+  basketController.deleteByBasketId,
 );
+
 export { basketRouter };
